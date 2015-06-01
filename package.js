578d43439c0e0a0ddc2a9dbb03f594695a74f2c5
Gadi Cohen <dragon@wastelands.net>
2015-06-01 17:49:31 +0200

    bump versions

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'gadicohen:famous',
-  version: '0.5.0',
+  version: '0.5.0_4',
   summary: 'Temporary package for Famo.us Mixed Mode v0.5.0',
   git: 'https://github.com/gadicc/meteor-famous',
 });
 
 Package.onUse(function(api) {
-  api.use('gadicohen:modules@0.0.1', 'client');
+  api.use('gadicohen:modules@0.0.5', 'client');
   api.addFiles('lib/modules.require', 'client');
   api.export('famous', 'client');
 });
